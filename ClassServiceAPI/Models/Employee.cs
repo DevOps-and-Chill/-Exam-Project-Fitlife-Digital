3a53a979b7997447320f77cec0085504d988e1d7
GittyHjalte <hjaltebording53@gmail.com>
2026-05-12 10:15:08 +0200

    Class.cs, Member.cs, Employee.cs, ExerciseGym.cs, Room.cs

---
+++ ClassServiceAPI/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassServiceAPI.Models;
+
+public class Employee
+{
+    public Guid Id { get; set; }
+    public string GivenName { get; set; } = string.Empty;
+    public string FamilyName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Telephone { get; set; } = string.Empty;
+}
---
+++ ClassServiceAPI/Models/Room.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassServiceAPI.Models;
+
+public class Room
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
