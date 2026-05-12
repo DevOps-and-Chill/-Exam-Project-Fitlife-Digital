56b1bc3328b29826651eae6a2b2dd5ed4a788d64
Anders <anders.otte@gmail.com>
2026-05-12 11:40:45 +0200

    dev UserService

---
+++ UserService/UserService/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using UserServiceAPI.Models;
+using UserServiceAPI.Repositories;
+using UserServiceAPI.Repositories.Interfaces;
+
+namespace UserServiceAPI.Controllers
+{
+    [ApiController]
+    [Route("user")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet(Name = "GetAllUsers")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("GetByAffiliation/{exerciseGymId}")]
+        public ActionResult<List<User>> GetUsersByAffiliation(Guid exerciseGymId)
+        {
+            var users = _userRepository.GetUsersByExerciseGym.
+                .Where(u => u.Affiliation == exerciseGymId)
+                .ToList();
+
+            return Ok(users);
+        }
+    }
+}
---
+++ UserService/UserService/Models/Employee.cs
@@ -0,0 +1,47 @@
+﻿using UserServiceAPI.Models.Enums;
+
+namespace UserServiceAPI.Models
+{
+    public class Employee : User
+    {
+        public EmployeeRole EmployeeRoleName { get; set; }
+        public bool isPT { get; set; }
+        public DateTime StartDate { get; init; }
+
+        public DateTime EndDate { get; set; }
+
+        public bool ActiveEmployment { get; set; } = true;
+
+        public Employee(
+            UserRole userRole,
+            string givenName,
+            string familyName,
+            string address,
+            string telephone,
+            string email,
+            Guid affiliation,
+            bool activeUser,
+            EmployeeRole roleName,
+            bool isPT,
+            DateTime endDate)
+            : base(
+                  userRole,
+                  givenName,
+                  familyName,
+                  address,
+                  telephone,
+                  email,
+                  affiliation,
+                  activeUser)
+        {
+            EmployeeRoleName = roleName;
+            this.isPT = isPT;
+            EndDate = endDate;
+        }
+
+        public void EndEmployment()
+        {
+            ActiveEmployment = false; 
+        }
+    }
+}
