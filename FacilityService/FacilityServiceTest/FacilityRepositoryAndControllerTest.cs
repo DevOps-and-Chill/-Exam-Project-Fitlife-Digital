4c784bb256bad178ff83021a5d4359f442e414ea
Jacob Ladegaard Friis Lageri <jaco7925@edu.eal.dk>
2026-05-12 14:59:56 +0200

    Added testing to the solution

---
+++ FacilityService/FacilityServiceTest/FacilityRepositoryAndControllerTest.cs
@@ -0,0 +1,18 @@
+﻿namespace FacilityServiceTest
+{
+    [TestClass]
+    public sealed class FacilityRepositoryAndControllerTest
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            
+        }
+
+        [TestMethod(DisplayName ="GetFacility")]
+        public void TestGetFacilityFlow()
+        {
+
+        }
+    }
+}
