4892b5447f7f2f015119c0fc9eed8b295910301b
Hjalte <hjaltebording53@gmail.com>
2026-05-17 11:04:29 +0200

    Oprettet NotificationController (uden endpoints)

---
+++ NotificationService/Controllers/NotificationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotificationService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+
+public class NotificationController : ControllerBase
+{
+    private readonly INotificationRepository _repo;
+
+
+    public NotificationController(INotificationRepository repo)
+    {
+        _repo = repo;
+    }
+    
+    // POST
+    
+    // GET
+    
+}
