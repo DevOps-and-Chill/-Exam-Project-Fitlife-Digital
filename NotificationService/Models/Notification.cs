b8f4223cb6b64770f13c0e8e2fa4901dc84fd41c
Hjalte <hjaltebording53@gmail.com>
2026-05-17 11:34:09 +0200

    Tilføjet User og Message models

--- NotificationService/Models/Notification.cs
+++ NotificationService/Models/Notification.cs
@@ -6,6 +6,7 @@ public class Notification
     public int? RecieverId { get; set; }
     public int? SenderId { get; set; }
     
+    public string Title { get; set; }
     public string Content { get; set; }
     public DateTime TimeStamp { get; set; }
 }
