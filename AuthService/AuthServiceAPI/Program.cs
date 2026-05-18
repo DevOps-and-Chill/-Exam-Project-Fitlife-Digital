ac8f9912598ae58aaaaf0dc69435ef550120fd63
Anders <anders.otte@gmail.com>
2026-05-18 20:23:02 +0200

    Dev #27
    Implemented DBContext, refactored AuthUser, configured EF/CosmosDB

--- AuthService/AuthServiceAPI/Program.cs
+++ AuthService/AuthServiceAPI/Program.cs
@@ -1,3 +1,7 @@
+using AuthServiceAPI.Data;
+using AuthServiceAPI.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,26 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddScoped<IAuthRepository, AuthRepositoryDB>();
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseCosmos(
+        builder.Configuration["CosmosDb:AccountEndpoint"]!,
+        builder.Configuration["CosmosDb:AccountKey"]!,
+        builder.Configuration["CosmosDb:DatabaseName"]!
+    );
+});
+
 var app = builder.Build();
+
+//AO: Ensures DB and container exists. EnsureCreated() is alternative of migration
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AuthDbContext>();
+
+    await db.Database.EnsureCreatedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
