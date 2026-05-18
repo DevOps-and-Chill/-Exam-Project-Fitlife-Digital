846f5bfceee72cf9ff35bebc768510307e50353c
Anders <anders.otte@gmail.com>
2026-05-18 15:13:10 +0200

    init

--- UserService/UserService/Repositories/DB Repos/EmployeeRepositoryDB.cs
+++ UserService/UserService/Repositories/DB Repos/EmployeeRepositoryDB.cs
@@ -13,7 +13,11 @@ namespace UserServiceAPI.Repositories
         {
             _context = context;
         }
-
+        /// <summary>
+        /// Finds employee based on id and removes from DB.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>Returns object that has been removed</returns>
         public async Task<Employee> DeleteEmployee(string userId)
         {
             var employee = await _context.Users
@@ -32,6 +36,11 @@ namespace UserServiceAPI.Repositories
             return employee;
         }
 
+        /// <summary>
+        /// Sets the property ActiveEmployment as false  
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>Returns updated employee-object</returns>
         public async Task<Employee> EndEmploymentForEmployee(string userId)
         {
             var employee = await _context.Users
@@ -50,11 +59,20 @@ namespace UserServiceAPI.Repositories
             return employee;
         }
 
+        /// <summary>
+        /// Used to get a list of all employees in the db
+        /// </summary>
+        /// <returns>List of employees</returns>
         public async Task<List<Employee>> GetAllEmployees()
         {
             return await _context.Users.OfType<Employee>().ToListAsync();
         }
 
+        /// <summary>
+        /// Used to set the property ActiveUser to false
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>Updated employee-object</returns>
         public async Task<Employee> SetAccountAsInactive(string userId)
         {
             var employee = await _context.Users
@@ -73,6 +91,11 @@ namespace UserServiceAPI.Repositories
             return employee;
         }
 
+        /// <summary>
+        /// Sets the property employeeRole to Manager
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>Returns the updated employee</returns>
         public async Task<Employee> SetEmployeeAsManager(string userId)
         {
             var employee = await _context.Users
