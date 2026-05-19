72e85bd70659d231583a747dff12076afb599792
Anders <anders.otte@gmail.com>
2026-05-19 21:43:13 +0200

    Attempt at fixing merge-conflict for NLogging

--- AuthService/AuthServiceAPI/Program.cs
+++ AuthService/AuthServiceAPI/Program.cs
@@ -7,72 +7,89 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using NLog;
+using NLog.Web; 
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
-// Add services to the container.
+logger.Debug("AuthService starter op");
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.AddScoped<ICredentialRepository, CredentialRepositoryDB>();
-builder.Services.AddScoped<IPasswordService, PasswordService>();
-
-builder.Services.AddDbContext<CredentialDbContext>(options =>
+try
 {
-    options.UseCosmos(
-        builder.Configuration["CosmosDb:AccountEndpoint"]!,
-        builder.Configuration["CosmosDb:AccountKey"]!,
-        builder.Configuration["CosmosDb:DatabaseName"]!
-    );
-});
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+
+    builder.Services.AddScoped<ICredentialRepository, CredentialRepositoryDB>();
+    builder.Services.AddScoped<IPasswordService, PasswordService>();
+
+    builder.Services.AddDbContext<CredentialDbContext>(options =>
     {
-        options.TokenValidationParameters =
-            new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-
-                ValidIssuer =
-                    builder.Configuration["Jwt:Issuer"],
-
-                ValidAudience =
-                    builder.Configuration["Jwt:Audience"],
-
-                IssuerSigningKey =
-                    new SymmetricSecurityKey(
-                        Encoding.UTF8.GetBytes(
-                            builder.Configuration["Jwt:Key"]))
-            };
+        options.UseCosmos(
+            builder.Configuration["CosmosDb:AccountEndpoint"]!,
+            builder.Configuration["CosmosDb:AccountKey"]!,
+            builder.Configuration["CosmosDb:DatabaseName"]!
+        );
     });
 
-var app = builder.Build();
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters =
+                new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
 
-//AO: Ensures DB and container exists. EnsureCreated() is alternative of migration
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<CredentialDbContext>();
+                    ValidIssuer =
+                        builder.Configuration["Jwt:Issuer"],
 
-    await db.Database.EnsureCreatedAsync();
+                    ValidAudience =
+                        builder.Configuration["Jwt:Audience"],
+
+                    IssuerSigningKey =
+                        new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(
+                                builder.Configuration["Jwt:Key"]))
+                };
+        });
+
+    var app = builder.Build();
+
+    //AO: Ensures DB and container exists. EnsureCreated() is alternative of migration
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<CredentialDbContext>();
+
+        await db.Database.EnsureCreatedAsync();
+    }
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
 }
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+catch (Exception ex)
 {
-    app.MapOpenApi();
+    // Logger fejl hvis applikationen crasher ved opstart
+    logger.Error(ex, "AuthService stoppede på grund af en fejl!");
+    throw;
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+finally
+{
+    // Sørg for at alle logs bliver skrevet færdigt før applikationen lukker
+    NLog.LogManager.Shutdown();
+}
