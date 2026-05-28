07542d2ede3e060ccd34573b692529b985de9260
Jacob Lageri <jacob.Lageri@hotmail.com>
2026-05-29 01:14:14 +0200

    Added "Administrer Center" page

--- FacilityService/FacilityServiceAPI/Models/OpeningHoursSpecification.cs
+++ FacilityService/FacilityServiceAPI/Models/OpeningHoursSpecification.cs
@@ -2,6 +2,7 @@
 {
 	public class OpeningHoursSpecification
 	{
+		public string Id { get; set; } = Guid.NewGuid().ToString();
 		public DayOfWeek DayOfWeek { get; set; }
 		public string Opens { get; set; }
 		public string Closes { get; set; }
