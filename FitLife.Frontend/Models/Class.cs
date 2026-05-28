a35df7baa5c3a4e47dbb245168c46d5fef68c0f3
Hjalte Bording <hjaltebording53@gmail.com>
2026-05-28 20:04:18 +0200

    Isactive = true

--- FitLife.Frontend/Models/Class.cs
+++ FitLife.Frontend/Models/Class.cs
@@ -5,16 +5,14 @@ public class Class
     public string Id { get; set; } = Guid.NewGuid().ToString();
     
     public string PartitionKey { get; set; } = "classes";
-    
     public string Title { get; set; } = "";
-    
     
     // (e.g. kl. 14:00 - 16:00)
     public DateTime TimeStart { get; set; }
     public DateTime TimeEnd { get; set; }
 
     public int MemberLimit { get; set; }
-    public bool ActiveClass { get; set; }
+    public bool ActiveClass { get; set; } = true;
     
     public bool IsUserSignedUp { get; set; }
     
